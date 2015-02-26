3c4478c08f62cc0b179158bad67a842f86336427
Olivier Mengué <dolmen@cpan.org>
2015-02-27 00:16:25 +0100

    Move the code from the script to the module

--- lib/App/json2perl.pm
+++ lib/App/json2perl.pm
@@ -5,4 +5,24 @@ package App::json2perl;
 
 our $VERSION = '0.01';
 
+
+use JSON::MaybeXS qw<decode_json>;
+use Data::Dumper  qw<Dumper>;
+
+
+sub run
+{
+    my @args = @_;
+    binmode(STDIN, ':raw');
+    my $data = decode_json do { local $/; <STDIN> };
+
+    local $Data::Dumper::Indent = 0;
+    local $Data::Dumper::Purity = 0;
+    local $Data::Dumper::Terse = 1;
+    local $Data::Dumper::Sortkeys = 1;
+    local $Data::Dumper::Quotekeys = 0;
+
+    print Dumper($data), "\n";
+}
+
 1;
