4c54d68ef8e562d9cb3b21a514a50ad6682d8ecb
Olivier Mengué <dolmen@cpan.org>
2015-02-27 17:59:52 +0100

    Rename the dist to App-JSON-to; rewrite to have a plugin system

---
+++ lib/App/JSON/to.pm
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+
+package App::JSON::to;
+# ABSTRACT: Convert JSON data to various formats
+
+our $VERSION = '0.01';
+
+use JSON::MaybeXS qw<decode_json>;
+
+sub run
+{
+    my ($to, @args) = @_;
+    $to = lc $to;
+
+    die "missing target format\n" unless defined $to;
+    die "invalid target format '$to'\n"
+	unless $to =~ /\A[a-z]+\z/ && eval { require "App/JSON/to/$to.pm"; 1 };
+    my $class = __PACKAGE__ . '::' . $to;
+    my $obj = $class->can('new') ? $class->new : $class;
+
+    # TODO parse options
+    # GetOptions($obj->options);
+
+    binmode(STDIN, ':raw');
+    my $data = decode_json do { local $/; <STDIN> };
+
+    print $obj->dump($data);
+}
+
+1;
