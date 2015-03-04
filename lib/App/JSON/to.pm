2c4e5143356812eabf809166ae3691ebb411c9ea
Olivier Mengué <dolmen@cpan.org>
2015-03-05 00:32:58 +0100

    Add minimal documentation in App::JSON::to

--- lib/App/JSON/to.pm
+++ lib/App/JSON/to.pm
@@ -33,3 +33,29 @@ sub run
 }
 
 1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::JSON::to - Backend module for the C<json-to> script
+
+=head1 DESCRIPTION
+
+See the L<json-to> script.
+
+=head1 AUTHOR
+
+Olivier MENGUÉ, L<mailto:dolmen@cpan.org>.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Olivier MENGUÉ.
+
+This is free software; you can redistribute it and/or modify it under the
+same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
